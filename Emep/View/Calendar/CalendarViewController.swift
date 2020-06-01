83106fd65fc0c0ae5f96129a3f7d015699908015
HFPing <hfp.ing2@gmail.com>
2020-06-01 10:30:13 -0400

    Timeline cell and header format

--- Emep/View/Calendar/CalendarViewController.swift
+++ Emep/View/Calendar/CalendarViewController.swift
@@ -33,6 +33,9 @@ class CalendarViewController: UITableViewController {
         let timelineTableViewCellNib = UINib(nibName: "TimelineTableViewCell",
         bundle: Bundle(url: nibUrl!)!)
         tableView.register(timelineTableViewCellNib, forCellReuseIdentifier: "TimelineTableViewCell")
+        tableView.tableFooterView = UIView()
+        tableView.sectionHeaderHeight = UITableView.automaticDimension
+        tableView.estimatedSectionHeaderHeight = 36
     }
 
     // MARK: - Table view data source
@@ -44,9 +47,19 @@ class CalendarViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return timelineViewModel.getNumberOfAppointments(forSection: section)
     }
+
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.separatorInset = .zero
+    }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return "Day " + String(describing: section + 1)
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView()
+        let label = UILabel()
+        label.text = timelineViewModel.getHeaderStringForSection(section)
+        label.font = UIFont.systemFont(ofSize: 32)
+        label.textAlignment = .center
+        label.addToAndFill(parent: view)
+        return view
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -55,41 +68,31 @@ class CalendarViewController: UITableViewController {
         }
 
         let cellViewModel = timelineViewModel.getAppointment(forIndexPath: indexPath)
-        cell.timelinePoint = TimelinePoint()
-        cell.timeline.frontColor = .clear
-        cell.timeline.backColor = .black
-        cell.titleLabel.text = cellViewModel.title
-        cell.descriptionLabel.text = cellViewModel.description
-        cell.lineInfoLabel.text = "Line info Label"
-        /*
-        let (timelinePoint, timelineBackColor, title, description, lineInfo, thumbnails, illustration) = sectionData[indexPath.row]
-        var timelineFrontColor = UIColor.clear
-        if (indexPath.row > 0) {
-            timelineFrontColor = sectionData[indexPath.row - 1].1
-        }
-        cell.timelinePoint = timelinePoint
-        cell.timeline.frontColor = timelineFrontColor
-        cell.timeline.backColor = timelineBackColor
-        cell.titleLabel.text = title
-        cell.descriptionLabel.text = description
-        cell.lineInfoLabel.text = lineInfo
+        cell.timelinePoint = TimelinePoint(diameter: 16.0, lineWidth: 4.0, color: UIColor(red: 0, green: 173, blue: 184, alpha: 1), filled: true)
+        cell.timeline = Timeline(width: 4.0, frontColor: .gray, backColor: .gray)
+        cell.timeline.frontColor = cellViewModel.isTop ? .clear : .systemGray3
+        cell.timeline.backColor = cellViewModel.isBottom ? .clear : .systemGray3
+
+        let textContent = cellViewModel.getTitle()
+
+        cell.titleLabel.text = textContent.title
+        cell.descriptionLabel.text = textContent.description
         
-        if let thumbnails = thumbnails {
-            cell.viewsInStackView = thumbnails.map { thumbnail in
-                return UIImageView(image: UIImage(named: thumbnail))
-            }
+        var imageResName = "ADN"
+        switch cellViewModel.getAppointmentType() {
+        case .fracture: imageResName = "Silla-ruedas"
+        case .checkup: imageResName = "Check"
+        case .operation: imageResName = "Telefono-beat"
+        case .vaccine: imageResName = "Jeringa"
+        default: break
         }
-        else {
-            cell.viewsInStackView = []
+
+        if cellViewModel.getAppointmentType() != .undefined {
+            cell.imageView?.image = UIImage(imageLiteralResourceName: imageResName)
+            cell.imageView?.contentMode = .scaleAspectFit
+            cell.imageView?.constrainToVerticalCenterAndLeading(leadingInset: 12)
+            cell.imageView?.constraintToSize(width: 40, height: 40)
         }
-        
-        if let illustration = illustration {
-            cell.illustrationImageView.image = UIImage(named: illustration)
-        }
-        else {
-            cell.illustrationImageView.image = nil
-        }
-        */
         return cell
     }
 }
