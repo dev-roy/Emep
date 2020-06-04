c6cd9656e83e72e542a6cc3263708bf9e5f7c835
dev-roy <rodrigo.ramos@enhanceit.us>
2020-06-04 11:56:43 -0400

    Add PrimaryInfoSignUpVC and ContactInfoSignUpVC

---
+++ Emep/View/Sign Up/ContactInfoSignUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ContactInfoSignUpViewController.swift
+//  Emep
+//
+//  Created by Rodrigo Buendia Ramos on 6/4/20.
+//  Copyright © 2020 Rodrigo Buendia Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ContactInfoSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
