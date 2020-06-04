185b4fc6b1710c719aef058f21b7046830c20b6f
dev-roy <rodrigo.ramos@enhanceit.us>
2020-06-04 12:22:54 -0400

    Add validation to PrimaryInfoSignUpVC

--- Emep/View/Sign Up/PrimaryInfoSignUpViewController.swift
+++ Emep/View/Sign Up/PrimaryInfoSignUpViewController.swift
@@ -7,24 +7,49 @@
 //
 
 import UIKit
+import ValidationTextField
 
 class PrimaryInfoSignUpViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var nameTextField: ValidationTextField!
+    @IBOutlet weak var lastNameTextField: ValidationTextField!
+    @IBOutlet weak var dobTextField: ValidationTextField!
+    @IBOutlet weak var policyNumberTextField: ValidationTextField!
+    
+    @IBOutlet weak var nextBtn: UIButton!
+    // MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addTargets()
+        setUpValidationTextFields()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func addTargets() {
+        nameTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        lastNameTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        dobTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
+        policyNumberTextField.addTarget(self, action: #selector(formValidation), for: .editingChanged)
     }
-    */
-
+    
+    private func setUpValidationTextFields() {
+        nameTextField.validCondition = {$0.count > 1 }
+        lastNameTextField.validCondition = {$0.count > 1 && $0.contains(" ")}
+        dobTextField.validCondition = {$0.count > 3}
+        policyNumberTextField.validCondition = {$0.count > 7}
+    }
+    
+    // MARK: - Handlers
+    @objc func formValidation() {
+        guard
+            nameTextField.isValid,
+            lastNameTextField.isValid,
+            dobTextField.isValid else {
+                nextBtn.isEnabled = false
+                nextBtn.alpha = 0.5
+                return
+        }
+        nextBtn.isEnabled = true
+        nextBtn.alpha = 1.0
+    }
 }
